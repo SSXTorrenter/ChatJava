798dad98dd7132ec2379244989ea06103f937305
thomas <thomas@192.168.163.212>
2016-05-06 08:25:47 +0200

    modifying host address in FrmLoadingTest.java

--- src/test/java/presentation/FrmLoadingTest.java
+++ src/test/java/presentation/FrmLoadingTest.java
@@ -16,13 +16,13 @@ public class FrmLoadingTest {
     
     @Test
     public void test_connection_to_db_true(){
-        boolean getCon = FrmLoading.getConnection("192.168.1.205",3306);
+        boolean getCon = FrmLoading.getConnection("ssxtorrenter.ddns.net",3306);
         assertTrue(getCon);
     }
     
     @Test
     public void test_connection_to_db_false(){
-        boolean getCon = FrmLoading.getConnection("192.168.1.206",3306);
+        boolean getCon = FrmLoading.getConnection("ssxtorrenter.ddns.com",3306);
         assertFalse(getCon);
     }
 }
